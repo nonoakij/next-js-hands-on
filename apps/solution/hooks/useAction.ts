8c4365f100f6666cf91b3cf1b6a22ad91f42ef3a
nonoakij <45055030+nonoakij@users.noreply.github.com>
2024-05-24 00:53:47 +0900

    chore: Add useAction hook for handling async actions
    ```

---
+++ apps/solution/hooks/useAction.ts
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+function useAction<T extends (arg: any) => Promise<any>>(action: T) {
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = async (arg: Parameters<T>) => {
+    setIsLoading(true);
+    const res = await action(arg);
+    setIsLoading(false);
+    return res;
+  };
+  return [dispatch, isLoading] as const;
+}
